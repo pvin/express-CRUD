bacf8070d01c6da5baa1827f9008c6dae3fe95b7
Praaveen <praaveen.vr@spritle.com>
2018-02-23 12:37:52 +0530

    Updated operation

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 
+app.use(express.static('public'))
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
@@ -26,18 +28,40 @@ MongoClient.connect('mongodb://name:password@ds239968.mlab.com:39968/share', (er
     })
 })
 
-app.post('/quotes', (req, res) => {
-    db.collection('quotes').save(req.body, (err, result) => {
-        if (err) return console.log(err)
-        console.log('saved to database')
-        res.redirect('/')
-
-})
-})
 
 app.get('/',(req, res) => {
     db.collection('quotes').find().toArray((err, result) => {
     if(err) return console.log(err)
     res.render('index.ejs',{quotes: result})
     })
+})
+
+app.post('/quotes', (req, res) => {
+    db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+    console.log('saved to database')
+res.redirect('/')
+
+})
+})
+
+
+app.put('/quotes', (req, res) => {
+    db.collection('quotes').
+    findOneAndUpdate({name: 'test3'}, {
+        $set: {
+            name: req.body.name,
+            quote: req.body.quote
+        }
+    } ,{
+              sort: {_id:-1},
+              upsert: true
+          },
+          (err, result) => {
+              if (err) return res.send(err)
+              res.send(result)
+           }
+
+
+    )
 })
